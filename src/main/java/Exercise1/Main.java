a1f721718b5a0584cebbe9cf6ce44349d413a8bb
Esther <githubEsther@gmail.com>
2025-10-17 17:54:49 +0200

    Feat: New generic and Main classes for the first exercise

---
+++ src/main/java/Exercise1/Main.java
@@ -0,0 +1,11 @@
+package Exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        NoGenericMethods noGeneric1 = new NoGenericMethods<>("pera",8,'o');
+
+        NoGenericMethods noGeneric2 = new NoGenericMethods<>(88,"poma",'A');
+
+
+    }
+}
