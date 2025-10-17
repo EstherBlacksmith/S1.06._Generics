9b2510b21846751ec47e98fc5932167a4112e84e
Esther <githubEsther@gmail.com>
2025-10-17 18:25:31 +0200

    Feat: Second level, one generic argument now is no generic

--- src/main/java/level1/Exercise2/GenericMethods.java
+++ src/main/java/level1/Exercise2/GenericMethods.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class GenericMethods<T> {
     private T property1;
     private T property2;
-    private T property3;
+    private Person property3;
 
     public GenericMethods(T property1, T property2, T property3) {
         this.property1 = Objects.requireNonNull(property1);
         this.property2 = Objects.requireNonNull(property2);
-        this.property3 = Objects.requireNonNull(property3);
+        this.property3 = (Person) Objects.requireNonNull(property3);
     }
 
     protected void setProperty1(T property1) {
@@ -22,7 +22,7 @@ public class GenericMethods<T> {
     }
 
     protected void setProperty3(T property3) {
-        this.property3 = property3;
+        this.property3 = (Person) property3;
     }
 
     protected void printParameters() {
